0c1b2a5b84a6fd19f1d506d111c57b237dbd0752
Polishchuk Denis <denis.p@partizan.ua>
2022-06-10 09:39:20 +0300

    Initial commit

---
+++ Source/ApplePicker/SortableListView.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ListView.h"
+#include "SortableListView.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APPLEPICKER_API USortableListView : public UListView
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "Sort")
+	void Sort() 
+	{
+		int32 size = this->ListItems.Num();
+		std::cout << size;
+		if (size > 0)
+		{
+			ULogData* tmp = Cast<ULogData>(this->ListItems[0].Get());
+			std::cout << tmp;
+		}
+	}
+};
